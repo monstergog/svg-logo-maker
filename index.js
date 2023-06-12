6193bd57d431890ca66685b4f063012ef304b81f
monstergog <amn002@ucsd.edu>
2023-06-12 16:35:17 -0700

    svg logo generator functionality mostly complete (tine tuning + edge case testing left)

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const {Shape, Circle, Square, Triangle} = require('./lib/shapes.js');
 const svgGen = require('./lib/generateSVG.js');
 const questions = require('./lib/questions.js')
 
-function renderShape(shape) {
-    switch(shape) {
-        case 'circle':
-            return ''
-            break;
-        case 'square':
-            return ''
-            break;
-        case 'triangle':
-            return ''
-            break;
-    }
-}
-
-
-function writeSVGFile(fileName, text, shape) {
+function writeSVGFile(fileName, data) {
     fs.writeFileSync(fileName, svgGen(data))
 }
 
 function init() {
     inquirer.prompt(questions)
     .then(((data) => {
-        writeSVGFile('logo.svg', text, renderShape(data.shape, data.shapeColor));
+        console.log(data);
+        writeSVGFile('logo.svg', data);
+        console.log('Generated logo.svg')
     }))
 }
 
