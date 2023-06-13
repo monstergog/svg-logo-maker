1519125afd94011a19e8ef657805adb59110ea4d
monstergog <amn002@ucsd.edu>
2023-06-12 21:59:15 -0700

    added comments

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// Import all required files
 const fs = require('fs');
 const inquirer = require('inquirer');
 const svgGen = require('./lib/generateSVG.js');
 const questions = require('./lib/questions.js')
 
+// Function to write to a file using passed in user input
 function writeSVGFile(fileName, data) {
     fs.writeFileSync(fileName, svgGen(data))
 }
 
+// Function to prompt user for input and sent to be written
 function init() {
     inquirer.prompt(questions)
     .then(((data) => {
